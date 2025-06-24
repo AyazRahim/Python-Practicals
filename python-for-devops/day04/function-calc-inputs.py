d82a55b40659c3a382bd00008dc0b52ed713dc6c
Ayaz Rahim <ayaz.raheem@prosoftwaretech.com>
2025-06-24 23:24:00 +0500

    day 04 practice and learning

---
+++ python-for-devops/day04/function-calc-inputs.py
@@ -0,0 +1,26 @@
+
+def addition(num1, num2):
+    add = num1 + num2
+    return  add
+
+def subtraction(num1, num2):
+    sub = num1 - num2
+    return  sub
+
+def multiplication(num1, num2):
+    mul = num1 * num2
+    return  mul
+
+def division(num1, num2):
+    div = num1 / num2
+    return  div
+
+addition(10 , 5)
+subtraction(10, 5)
+multiplication(10, 5)
+division(10, 5)
+
+print("Addition:", addition(10, 5))
+print("Subtraction:", subtraction(10, 5 ))
+print("Multiplication:", multiplication(10, 5))
+print("Division:", division(10, 5))
---
+++ python-for-devops/day04/function-calculator.py
@@ -0,0 +1,19 @@
+num1 = 10
+num2 = 5
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def subtraction():
+    sub = num1 - num2
+    print(sub)
+
+def mulitplication():
+    mul = num1 * num2
+    print(mul)
+
+addition()
+subtraction()
+mulitplication()
+
